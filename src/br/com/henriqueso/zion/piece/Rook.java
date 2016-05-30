2b05ff0fba0869cdf42a53081dc5b98854ce6453
Rafael Cruz <rafinha.cruz7@gmail.com>
2016-05-30 18:20:21 -0300

    Adding Rook implementation
    Adding threats and chess board positions

--- src/br/com/henriqueso/zion/piece/Rook.java
+++ src/br/com/henriqueso/zion/piece/Rook.java
@@ -9,7 +9,67 @@ public class Rook extends ChessPiece {
 
 	@Override
 	public List<Position> threatens(ChessBoard chessBoard) {
-		return null;
+		List<Position> threatened = new ArrayList<>();
+		
+		Position newPosition = null;
+		
+		for(int row = 0; row <= chessBoard.getRows; row++) {
+			newPosition = top(row);
+			if(chessBoard.isValidPosition(newPosition)) {
+				threatened.add(newPosition);
+			}
+		}
+		
+		for(int row = 0; row <= chessBoard.getRows; row++) {
+			newPosition = bottom(row);
+			if(chessBoard.isValidPosition(newPosition)) {
+				threatened.add(newPosition);
+			}
+		}
+		
+		for(int column = 0; column <= chessBoard.getColumns; column++) {
+			newPosition = left(column);
+			if(chessBoard.isValidPosition(newPosition)) {
+				threatened.add(newPosition);
+			}
+		}
+		
+		for(int column = 0; column <= chessBoard.getColumns; column++) {
+			newPosition = right(column);
+			if(chessBoard.isValidPosition(newPosition)) {
+				threatened.add(newPosition);
+			}
+		}
+		
+		return threatened;
+	}
+	
+	private Position top(int up) {
+		int threatenedX = getPosition().getX();
+		int threatenedY = getPosition().getY() + up;
+		
+		return new Position(threadtenedX, threatenedY);
+	}
+	
+	private Position bottom(int down) {
+		int threatenedX = getPosition().getX();
+		int threatenedY = getPosition().getY() - bottom;
+		
+		return new Position(threadtenedX, threatenedY);
+	}
+	
+	private Position left(int left) {
+		int threatenedX = getPosition().getX() - left;
+		int threatenedY = getPosition().getY();
+		
+		return new Position(threadtenedX, threatenedY);
+	}
+	
+	private Position right(int right) {
+		int threatenedX = getPosition().getX() + right;
+		int threatenedY = getPosition().getY();
+		
+		return new Position(threadtenedX, threatenedY);
 	}
 
 	@Override
